f1a5932a5c9c90e6cc84fe5ef806a5de1bf83cdc
rohitkapri-superatom <rohitchandra@superatom.ai>
2025-09-10 21:22:03 +0530

    fixed the default dsl to be shown at the center

--- apps/frontend/src/lib/utils/default-dsl.ts
+++ apps/frontend/src/lib/utils/default-dsl.ts
@@ -1,11 +1,34 @@
 export const default_dsl = {
-    ui:{
-        id: "ui_33O2Hf",
-        type: "div",
+  ui: {
+    id: "ui_33O2Hf",
+    type: "div",
+    props: {
+      className: `
+        min-h-screen          
+        bg-gray-50           
+        flex                
+        items-center         
+        justify-center      
+        p-4                    
+      `,
+      style: {
+        textAlign: "center", 
+      },
+    },
+    children: [
+      {
+        id: "welcome_text",
+        type: "h1",
         props: {
-            className: "min-h-screen bg-gray-50 py-8"
+          className: `
+            text-3xl             
+            font-bold             
+            text-gray-800       
+          `,
         },
         children: ["Welcome to Superatom"],
-    },
-    data:{}
-}
+      },
+    ],
+  },
+  data: {},
+};
