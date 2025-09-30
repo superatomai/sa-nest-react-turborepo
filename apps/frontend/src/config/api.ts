a7f7008c3b856437e9d9e2b45ce25cff9801e2b6
rohitkapri-superatom <rohitchandra@superatom.ai>
2025-09-30 11:05:27 +0530

    formatted the api.ts

--- apps/frontend/src/config/api.ts
+++ apps/frontend/src/config/api.ts
@@ -1,34 +1,30 @@
-const getApiEndpoint = ()=>{
+const getApiEndpoint = () => {
+  const vite_local_api_url = import.meta.env.VITE_LOCAL_API_URL;
+  const vite_dev_api_url = import.meta.env.VITE_API_URL;
+  const vite_prod_api_url = import.meta.env.VITE_PROD_API_URL;
 
-    const vite_local_api_url = import.meta.env.VITE_LOCAL_API_URL;
-    const vite_dev_api_url = import.meta.env.VITE_API_URL;
-    const vite_prod_api_url = import.meta.env.VITE_PROD_API_URL;
+  const branch = import.meta.env.VITE_CF_PAGES_BRANCH;
 
-    const branch = import.meta.env.VITE_CF_PAGES_BRANCH;
+  let apiUrl = vite_prod_api_url;
 
-    let apiUrl = vite_prod_api_url;
+  const isLocalHost =
+    window.location.hostname === "localhost" ||
+    window.location.hostname === "127.0.0.1";
 
+  // for local dev
+  if (isLocalHost) {
+    apiUrl = vite_local_api_url;
+  }
 
-    const isLocalHost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+  if (branch === "main") {
+    apiUrl = vite_prod_api_url;
+  } else if (branch === "dev") {
+    apiUrl = vite_dev_api_url;
+  }
 
-    // for local dev
-    if(isLocalHost){
-        apiUrl = vite_local_api_url;
-        
-    }
+  console.log("Using API URL:", apiUrl);
 
-
-    if(branch === 'main'){
-        apiUrl = vite_prod_api_url
-    }
-    else if(branch === 'dev'){
-        apiUrl = vite_dev_api_url
-    }
-    
-    console.log("Using API URL:", apiUrl);
-
-    return apiUrl;
-
-}
+  return apiUrl;
+};
 
 export const API_URL = getApiEndpoint();
