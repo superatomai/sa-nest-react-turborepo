c241615569019413cecfb594b8f7f9834225142f
rohitkapri-superatom <rohitchandra@superatom.ai>
2025-09-30 11:02:58 +0530

    changed the env variable spelling, prdo to prod

--- apps/frontend/src/config/api.ts
+++ apps/frontend/src/config/api.ts
@@ -2,11 +2,11 @@ const getApiEndpoint = ()=>{
 
     const vite_local_api_url = import.meta.env.VITE_LOCAL_API_URL;
     const vite_dev_api_url = import.meta.env.VITE_API_URL;
-    const vite_prdo_api_url = import.meta.env.VITE_PRDO_API_URL;
+    const vite_prod_api_url = import.meta.env.VITE_PROD_API_URL;
 
     const branch = import.meta.env.VITE_CF_PAGES_BRANCH;
 
-    let apiUrl = vite_prdo_api_url;
+    let apiUrl = vite_prod_api_url;
 
 
     const isLocalHost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
@@ -19,7 +19,7 @@ const getApiEndpoint = ()=>{
 
 
     if(branch === 'main'){
-        apiUrl = vite_prdo_api_url
+        apiUrl = vite_prod_api_url
     }
     else if(branch === 'dev'){
         apiUrl = vite_dev_api_url
