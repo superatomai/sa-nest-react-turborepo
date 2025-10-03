2f9287ca374beb80933ace370feb93bb24ac7f72
rohitkapri-superatom <rohitchandra@superatom.ai>
2025-10-03 11:25:56 +0530

    changed things in the api.ts using CF_PAGES_BRANCH

--- apps/frontend/src/config/api.ts
+++ apps/frontend/src/config/api.ts
@@ -3,7 +3,7 @@ const getApiEndpoint = () => {
   const vite_dev_api_url = import.meta.env.VITE_DEV_API_URL;
   const vite_prod_api_url = import.meta.env.VITE_PROD_API_URL;
 
-  const branch = import.meta.env.VITE_CF_PAGES_BRANCH;
+  const branch = import.meta.env.CF_PAGES_BRANCH || import.meta.env.VITE_CF_PAGES_BRANCH;
 
   const isLocalHost =
     window.location.hostname === "localhost" ||
